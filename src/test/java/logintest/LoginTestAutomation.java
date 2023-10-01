c19c00516bf96b45166d3a08406c680738d5fb6d
VKuz89 <vladislav.kuznetsov89@gmail.com>
2023-10-01 10:55:27 +0300

    Update code

--- src/test/java/logintest/LoginTestAutomation.java
+++ src/test/java/logintest/LoginTestAutomation.java
@@ -1,35 +1,21 @@
 package logintest;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class LoginTestAutomation {
     public static WebDriver driver = null;
-    public static DesiredCapabilities capabilities = null;
-
-
-    @Before
-    public void initialisation () {
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("version", "Version 114.0.5735.134 (Official Build) (64-bit)");
-        capabilities.setCapability("platform", "Microsoft Windows 10 Pro");
-        System.setProperty("webdriver.chrome.driver", "D://VLad//Selenium Jars and Drivers//Drivers//chromedriver/chromedriver.exe");
-
-    }
-
-
 
         @Test
         public void successfulLoginTest() throws InterruptedException {
+
             System.out.println("Successful login test to be executed...");
-                        //System.setProperty("webdriver.chrome.driver", "D://VLad//Selenium Jars and Drivers//Drivers//chromedriver/chromedriver.exe");
+                        System.setProperty("webdriver.chrome.driver", "D://VLad//Selenium Jars and Drivers//Drivers//chromedriver/chromedriver.exe");
                         WebDriver driver = new ChromeDriver();
                         driver.get("https://www.saucedemo.com/");
                         driver.manage().window().maximize();
@@ -38,7 +24,7 @@ public class LoginTestAutomation {
                         driver.findElement(By.name("password")).sendKeys("secret_sauce");
                         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 
-                        Thread.sleep(5000);
+                        Thread.sleep(2000);
                        String actualUrl = "https://www.saucedemo.com/inventory.html";
                        String expectedUrl = driver.getCurrentUrl();
                        Assert.assertEquals(expectedUrl, actualUrl);
